53607d3b2250f740f81ccd68b58e2a9955d4e49b
Adebukol <adebukolaomowole@gmail.com>
2022-08-13 20:59:17 +0100

    second commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// const myArray =[];
+// let i = 5;
+// while(i > -1){XMLHttpRequestEventTarget
+//   myArray.push(i);
+//   i--;
+// }
+// console.log(myArray);
+
+// //Action dropdown menu
+// function myFunction(){
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+
+// window.onclick = function(e){
+//   if(!e.target.matches(".dropbtn")){
+//       var myDropdown = document.getElementById("myDropdown");
+//       // var i;
+//       // for(i = 0; i<dropdowns.length; i++) {
+//       //     var openDropdown = dropdowns[i];
+//           if(myDropdown.classList.contains("show")){
+//               myDropdown.classList.remove("show");
+//           }
+
+//       }
+//   }
+//   //end
+//   $(".rotate").click(function () {
+//     $(this).toggleClass("down");
+// })
