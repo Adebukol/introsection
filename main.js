6db73b7f6ba5ecffc2c008a3dd0f7ee46db1478b
Adebukol <adebukolaomowole@gmail.com>
2022-08-25 20:44:13 +0100

    index file updated

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+function show(){
+  // document.getElementById("mouseup").style.transform = rotateZ("-180deg");
+  document.getElementById("myDropdown").style.display = "block";
+}
+
+function reveal(){
+  document.getElementById("class-list").style.display=" block";
+}
+window.onclick = function(e){
+  if(!e.target.matches(".show-features")){
+      var myDropdown = document.getElementById("myDropdown");
+      // var i;
+      // for(i = 0; i<dropdowns.length; i++) {
+      //     var openDropdown = dropdowns[i];
+          if(myDropdown.classList.contains("show")){
+              myDropdown.classList.remove("show");
+          }
+
+      }
+  }
 // const myArray =[];
 // let i = 5;
 // while(i > -1){XMLHttpRequestEventTarget
